d67d32cb3cf5ed3dc3096de1d797681340274b60
Michael Friendly <friendly@yorku.ca>
2019-02-02 15:19:13 -0500

    update DESCRIPTION and source-data .R file

--- source-data/TrumpLies.R
+++ source-data/TrumpLies.R
@@ -2,14 +2,17 @@ library(readr)
 library(dplyr)
 library(magrittr)
 library(lubridate)
+library(forcats)
 
-TrumpLies <- read_csv("Trumpcheck-YorkU-Dec5.csv",
+# NB: had to manually remove the blank lines ",,,,,,,," at the end of the file
+
+TrumpLies <- read_csv("source-data/Trumpcheck-YorkU-Dec5.csv",
 col_types = cols(date = col_date(format = "%m-%d-%Y")))
 
 
-TrumpLies <- TrumpLies %>% 
+TrumpLies <- TrumpLies %>%
     mutate(month=month(date), year=year(date)) %>%
-    mutate(`repeat` = as.factor(`repeat`), 
+    mutate(`repeat` = factor(`repeat`, labels=c("no", "yes")),
            category1 = as.factor(category1),
            category2 = as.factor(category2),
            category3 = as.factor(category3),
@@ -18,4 +21,24 @@ TrumpLies <- TrumpLies %>%
 
 glimpse(TrumpLies)
 
-save(TrumpLies, file="TrumpLies.RData")
+save(TrumpLies, file="data/TrumpLies.RData")
+
+# simple histogram
+library(ggplot2)
+library(scales)
+# to plot the density also, use trick from https://stat.ethz.ch/pipermail/r-help/2011-June/280588.html
+ggplot(TrumpLies, aes(x=date))  +
+  geom_histogram(binwidth=20, colour="black", fill="lightblue") +
+  geom_density(aes(y=30* ..count..), alpha=.2, fill="#FF6666") +
+  scale_x_date(labels = scales::date_format("%Y-%m"),
+               breaks = seq(min(TrumpLies$date)-5, max(TrumpLies$date)+5, 30)) +
+  ylab("Number of False Claims") + xlab("Year and Month") +
+  theme_bw() + theme(axis.text.x = element_text(angle=45, vjust = 1, hjust=1))
+
+
+ggplot(dates, aes(x=Date)) + geom_histogram(binwidth=30, colour="white") +
+  scale_x_date(labels = date_format("%Y-%b"),
+               breaks = seq(min(dates$Date)-5, max(dates$Date)+5, 30),
+               limits = c(as.Date("2008-05-01"), as.Date("2012-04-01"))) +
+  ylab("Frequency") + xlab("Year and Month") +
+  theme_bw() + opts(axis.text.x = theme_text(angle=90))
